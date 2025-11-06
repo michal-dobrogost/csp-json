643f713c2f8a3bcc58f2ca0291154fa47e9e07dd
Michal Dobrogost <michal.dobrogost@gmail.com>
2025-11-06 13:29:25 -0500

    gen-bipartite: link with libc.

--- tools/cj-gen-bipartite/build.zig
+++ tools/cj-gen-bipartite/build.zig
@@ -64,6 +64,7 @@ pub fn build(b: *std.Build) void {
     exe.addCSourceFiles(.{
         .files = &files,
     });
+    exe.linkLibC();
 
     // This declares intent for the executable to be installed into the
     // install prefix when running `zig build` (i.e. when executing the default
